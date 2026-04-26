11cbb9fbef12d78ff7d74c96ac53685aca2eefe5
XxAntoni0xX <1882901@alu.murciaeduca.es>
2026-04-26 15:53:03 +0200

    Actualizar JavaDriveTest.java

--- src/test/java/JavaDriveTest.java
+++ src/test/java/JavaDriveTest.java
@@ -9,45 +9,41 @@ import logica.*;
 public class JavaDriveTest {
 
     @Test
-    public void testCoberturaTotal() {
+    public void testSistemaCompleto() {
         
         GestorClientes gc = new GestorClientes();
         Cliente c1 = new Cliente("Antonio", "11111111A", "600000001");
-        Cliente c2 = new Cliente("Juan", "22222222B", "600000002");
         
         gc.addCliente(c1);
-        gc.addCliente(c2);
         
         
         assertNotNull(gc.buscarCliente("11111111A"));
-        
         assertNull(gc.buscarCliente("99999999Z"));
-        
-        assertEquals(2, gc.getClientes().size());
+        assertNotNull(gc.getClientes());
 
         
         GestorFlota gf = new GestorFlota();
-        Coche coche = new Coche("1234ABC", "Toyota", "Corolla", true, "Turismo", 5);
         
+        Coche cocheDisp = new Coche("1234ABC", "Toyota", "Corolla", true, "Turismo", 5);
         Coche cocheNoDisp = new Coche("9999ZZZ", "Fiat", "500", false, "Urbano", 4);
         
-        gf.addVehiculo(coche);
+        gf.addVehiculo(cocheDisp);
         gf.addVehiculo(cocheNoDisp);
         
         
         assertNotNull(gf.buscarVehiculo("1234ABC"));
-        assertNull(gf.buscarVehiculo("0000XXX"));
+        assertNull(gf.buscarVehiculo("XXXXXXX"));
         
-        )
+        
         gf.listarDisponibles();
-        assertEquals(2, gf.getFlota().size());
+        assertNotNull(gf.getFlota());
 
         
         LocalDate hoy = LocalDate.now();
-        Reserva r = new Reserva(c1, coche, hoy, hoy.plusDays(3));
+        Reserva r = new Reserva(c1, cocheDisp, hoy, hoy.plusDays(2));
         assertNotNull(r);
 
-      
+        
         assertNotNull(new GestorReservas());
         assertNotNull(new GestorInformes());
         assertNotNull(new GestorPersistencia());
