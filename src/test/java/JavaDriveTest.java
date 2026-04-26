338907f5145c674a17f03350d387681cb70bb123
XxAntoni0xX <1882901@alu.murciaeduca.es>
2026-04-26 14:19:16 +0200

    Actualizado JavaDriveTest.java

--- src/test/java/JavaDriveTest.java
+++ src/test/java/JavaDriveTest.java
@@ -3,31 +3,34 @@ package test;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
-
 import model.*;
 import logica.*;
 
 public class JavaDriveTest {
 
     @Test
-    public void testCoberturaTotal() {
+    public void testCoberturaEfectiva() {
         
         Cliente cliente = new Cliente("Antonio", "12345678Z", "600000000");
-        assertNotNull(cliente);
-
         
-        Coche coche = new Coche(); 
-        assertNotNull(coche);
+       
+        Coche coche = new Coche("1234ABC", "Toyota", "Corolla", true, "Turismo", 5);
 
         
         GestorClientes gc = new GestorClientes();
         GestorFlota gf = new GestorFlota();
         GestorReservas gr = new GestorReservas();
 
-        assertNotNull(gc);
-        assertNotNull(gf);
-        assertNotNull(gr);
+        
+        gc.agregarCliente(cliente); 
+        gf.agregarVehiculo(coche);
+        
+       
+        assertNotNull(gc.obtenerClientes());
+        assertNotNull(gf.obtenerVehiculos());
         
         
+        Reserva reserva = new Reserva(cliente, coche);
+        assertNotNull(reserva);
     }
 }
