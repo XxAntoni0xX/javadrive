b0417bf022204f282b5a8ec8f9f3803be6f4d3b6
XxAntoni0xX <1882901@alu.murciaeduca.es>
2026-04-26 16:04:42 +0200

    Actualizado JavaDriveTest.java

--- src/test/java/JavaDriveTest.java
+++ src/test/java/JavaDriveTest.java
@@ -15,27 +15,28 @@ public class JavaDriveTest {
     public void testSistemaCompleto() throws Exception {
         Cliente cl = new Cliente("11111111A", "Antonio", "600111222"); 
         Coche co = new Coche("1234ABC", "Toyota", "Corolla", true, "Híbrido", 5);
-        Furgoneta fuCarga = new Furgoneta("5678DEF", "Ford", "Transit", true, true, 1000);
-        Furgoneta fuPasajeros = new Furgoneta("9999XYZ", "VW", "Multivan", true, false, 9);
+        Furgoneta fuC = new Furgoneta("5678DEF", "Ford", "Transit", true, true, 1000);
+        Furgoneta fuP = new Furgoneta("9999XYZ", "VW", "Multivan", true, false, 9);
 
         GestorClientes gc = new GestorClientes();
         gc.addCliente(cl);
         gc.buscarCliente("11111111A");
-        gc.buscarCliente("00000000Z");
+        gc.buscarCliente("NO_EXISTE");
         gc.getClientes();
 
         GestorFlota gf = new GestorFlota();
         gf.addVehiculo(co);
-        gf.addVehiculo(fuCarga);
-        gf.addVehiculo(fuPasajeros);
+        gf.addVehiculo(fuC);
+        gf.addVehiculo(fuP);
         gf.buscarVehiculo("1234ABC");
-        gf.buscarVehiculo("NULL");
+        gf.buscarVehiculo("INEXISTENTE");
         gf.listarDisponibles();
         gf.getFlota();
 
         GestorReservas gr = new GestorReservas();
         gr.procesarReserva(cl, co);
-        gr.procesarReserva(null, fuCarga);
+        gr.procesarReserva(null, fuC);
+        gr.procesarReserva(cl, null);
         
         GestorInformes.generarInformeXML(gf.getFlota(), gc.getClientes());
 
@@ -46,17 +47,11 @@ public class JavaDriveTest {
         gp.cargarDatos(gf, gc);
 
         co.obtenerDetalles();
-        fuCarga.obtenerDetalles();
-        fuPasajeros.obtenerDetalles();
-        co.getMarca();
-        co.getModelo();
-        co.getMatricula();
-        co.isDisponible();
-        cl.getNombre();
-        cl.getDni();
-        cl.getTelefono();
-        cl.toString();
-        co.toString();
+        fuC.obtenerDetalles();
+        fuP.obtenerDetalles();
+        co.getMarca(); co.getModelo(); co.getMatricula(); co.isDisponible();
+        cl.getNombre(); cl.getDni(); cl.getTelefono();
+        cl.toString(); co.toString();
 
         Reserva res = new Reserva(cl, co, LocalDate.now(), LocalDate.now().plusDays(3));
         res.generarLineaTicket();
