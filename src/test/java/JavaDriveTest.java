77d92b92d83b79b8522a4a8638cd1643f7c486d3
XxAntoni0xX <1882901@alu.murciaeduca.es>
2026-04-26 13:54:39 +0200

    Create JavaDriveTest.java

---
+++ src/test/java/JavaDriveTest.java
@@ -0,0 +1,11 @@
+package test;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class JavaDriveTest {
+    @Test
+    public void testInicial() {
+        assertTrue(true);
+    }
+}
