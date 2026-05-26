937d4d259fcd02c0d1ba69ad8985797591fadefe
XxAntoni0xX <1882901@alu.murciaeduca.es>
2026-05-26 18:37:31 +0200

    Actualizar GestorReservas

--- src/test/java/logica/GestorReservasTest.java
+++ src/test/java/logica/GestorReservasTest.java
@@ -16,19 +16,40 @@ public class GestorReservasTest {
         
         boolean resultado = gr.procesarReserva(cl, co);
         
+       
         assertTrue(resultado);
-        assertFalse(co.isDisponible());
+        assertFalse(co.isDisponible()); 
         
-        new File("ticket_1234ABC.txt").delete();
+        
+        File ticket = new File("ticket_1234ABC.txt");
+        if (ticket.exists()) {
+            ticket.delete();
+        }
     }
 
     @Test
-    public void testProcesarReservaFallo() {
+    public void testProcesarReservaFalloVehiculoNoDisponible() {
         GestorReservas gr = new GestorReservas();
         Cliente cl = new Cliente("11111111A", "Antonio", "600111222");
         Coche coNoDisp = new Coche("9999XYZ", "Fiat", "500", false, "Urbano", 4);
         
+        
         assertFalse(gr.procesarReserva(cl, coNoDisp));
-        assertFalse(gr.procesarReserva(null, coNoDisp));
+    }
+
+    @Test
+    public void testProcesarReservaFalloParametrosNulos() {
+        GestorReservas gr = new GestorReservas();
+        Cliente cl = new Cliente("11111111A", "Antonio", "600111222");
+        Coche coDisp = new Coche("1234ABC", "Toyota", "Corolla", true, "Híbrido", 5);
+        
+       
+        assertFalse(gr.procesarReserva(null, coDisp));
+        
+        
+        assertFalse(gr.procesarReserva(cl, null));
+        
+        
+        assertFalse(gr.procesarReserva(null, null));
     }
 }
